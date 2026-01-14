376008612ba7c80fec05eeb48c2028f5b1d18d68
MFurkan02 <m.yilgin2021@gtu.edu.tr>
2026-01-15 00:22:05 +0300

    Project name,number removed from output

--- app.py
+++ app.py
@@ -430,11 +430,11 @@ def download(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 
+"""if __name__ == '__main__':
+    app.run(debug=True, port=5000)"""
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
 
-"""if __name__ == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use Render's port if available
-    app.run(host="0.0.0.0", port=port, debug=True)"""
+    app.run(host="0.0.0.0", port=port, debug=False)
 
